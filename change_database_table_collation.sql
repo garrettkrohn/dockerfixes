e3c3f9192d4f3e98213f3959c0e1fa3636f474d3
Garrett Krohn <gkrohn@kipsu.edu>
2022-11-16 11:50:21 -0600

    added comments for clarification

--- change_database_table_collation.sql
+++ change_database_table_collation.sql
@@ -1,3 +1,6 @@
+-- right now with docker we can get collation issues.  The collation needs to match between the tables and procedure
+
 -- just change the name of the table
 
 ALTER TABLE company CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
